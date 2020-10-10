50cc806a09c064275d4747b2b124877361cfdfff
Muhammad Ali <icecap360@live.com>
2020-10-10 13:53:09 -0400

    Initializing repository and camera sensor

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/muhammad/armws/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/muhammad/armws/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/muhammad/armws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/muhammad/armws/src:$ROS_PACKAGE_PATH"
