18aa7b49cf4868fd0ccbe2f4bd35f8c09e6c294b
Winnie T <WinnieTrandinh@users.noreply.github.com>
2020-10-11 18:01:09 -0400

    Revert "Restructured and renamed file conventions."
    This reverts commit 36d795cc872aca736c905b46863aacf153b68f54.

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/muhammad/armws/src
+# Build directory: /home/muhammad/armws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("Camera_description")
+subdirs("Camera_gazebo")
