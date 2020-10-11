36d795cc872aca736c905b46863aacf153b68f54
Winnie T <WinnieTrandinh@users.noreply.github.com>
2020-10-11 16:49:26 -0400

    Restructured and renamed file conventions.

--- build/Camera_gazebo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/muhammad/armws/src/Camera_gazebo
-# Build directory: /home/muhammad/armws/build/Camera_gazebo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
