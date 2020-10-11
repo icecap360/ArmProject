7d8d0582e02602d868274e3573c40f933f1329ba
Winnie T <WinnieTrandinh@users.noreply.github.com>
2020-10-11 18:04:18 -0400

    File restructure.

--- build/sensors/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/muhammad/armws/src/sensors
-# Build directory: /home/muhammad/armws/build/sensors
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
