eb7096c2689d52e917622368053ba85ab77583bd
Winnie T <WinnieTrandinh@users.noreply.github.com>
2020-12-30 13:32:58 -0500

    Restructured user_interface into classes.

--- src/control/src/control/user_interface.py
+++ src/control/src/control/user_interface.py
@@ -5,25 +5,22 @@ import rospy
 from control.msg import class_list
 from control.srv import isGo, isGoResponse
 
-pub = rospy.Publisher('desired_classes', class_list, queue_size=10)
+class user_interface:
+    def __init__(self):
+        self.pub = rospy.Publisher('desired_classes', class_list, queue_size=10)
+        self.serv = rospy.Service('is_go', isGo, self.is_go)
 
-# anonymous to append numbers to class_list to make it unique in case of collision
-rospy.init_node('desired_classes', anonymous=True)
-r = rospy.Rate(10)
+    # define service
+    def is_go(self, req):
+      return isGoResponse(1)
 
-# define service
-def is_go(req):
-  return isGoResponse(1)
-s = rospy.Service('is_go', isGo, is_go)
-
-# service call
-# wait_for_service(<name of service>)
-#rospy.wait_for_service('is_go')
-# ServiceProxy(<function to be called>, <service name from above import>)
-# makes service function into local function
-#is_go = rospy.ServiceProxy('is_go', isGo)
-
-while not rospy.is_shutdown():
-    pub.publish(["foo", "bar"])
-    #print(add_two_ints(1,2))
-    r.sleep();
+if __name__ == '__main__':
+    rospy.init_node('desired_classes', anonymous=True)
+    # call constructor
+    ui = user_interface()
+    # main loop
+    r = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        ui.pub.publish(["foo", "bar"])
+        #print(add_two_ints(1,2))
+        r.sleep();
