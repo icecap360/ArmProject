5af3b2311c1ac37636ac9c6ecb76a5922d362bb4
Winnie T <WinnieTrandinh@users.noreply.github.com>
2020-12-30 15:44:58 -0500

    desired_classes and is_go now use user input.

--- src/control/src/control/user_interface.py
+++ src/control/src/control/user_interface.py
@@ -4,15 +4,27 @@ import rospy
 # from std_msgs.msg import String
 from control.msg import class_list
 from control.srv import isGo, isGoResponse
+#import string
 
 class user_interface:
     def __init__(self):
         self.pub = rospy.Publisher('desired_classes', class_list, queue_size=10)
         self.serv = rospy.Service('is_go', isGo, self.is_go)
+        self.desired_classes = [""]
 
     # define service
     def is_go(self, req):
-      return isGoResponse(1)
+        return self.get_desired_classes()
+
+    def get_desired_classes(self):
+        self.desired_classes = raw_input("Enter space seperated desired classes: ").split()
+        return self.start()
+
+    def start(self):
+        if raw_input("Start robot? (y/n) ") == 'y':
+            return True
+        return False
+
 
 if __name__ == '__main__':
     rospy.init_node('desired_classes', anonymous=True)
@@ -21,6 +33,6 @@ if __name__ == '__main__':
     # main loop
     r = rospy.Rate(10)
     while not rospy.is_shutdown():
-        ui.pub.publish(["foo", "bar"])
+        ui.pub.publish(ui.desired_classes)
         #print(add_two_ints(1,2))
         r.sleep();
