e19b59b2f4f748c32b635ac996a10bc43725f0a9
Winnie T <WinnieTrandinh@users.noreply.github.com>
2020-12-30 12:50:17 -0500

    Added is_go service.

--- src/control/src/control/client_ex.py
+++ src/control/src/control/client_ex.py
@@ -3,27 +3,22 @@
 import rospy
 # from std_msgs.msg import String
 from control.msg import class_list
-from control.srv import isGo, isGoResponse
+from control.srv import isGo
 
 pub = rospy.Publisher('desired_classes', class_list, queue_size=10)
 
 # anonymous to append numbers to class_list to make it unique in case of collision
-rospy.init_node('desired_classes', anonymous=True)
+rospy.init_node('client', anonymous=True)
 r = rospy.Rate(10)
-
-# define service
-def is_go(req):
-  return isGoResponse(1)
-s = rospy.Service('is_go', isGo, is_go)
 
 # service call
 # wait_for_service(<name of service>)
-#rospy.wait_for_service('is_go')
+rospy.wait_for_service('is_go')
 # ServiceProxy(<function to be called>, <service name from above import>)
 # makes service function into local function
-#is_go = rospy.ServiceProxy('is_go', isGo)
+is_go = rospy.ServiceProxy('is_go', isGo)
 
 while not rospy.is_shutdown():
     pub.publish(["foo", "bar"])
-    #print(add_two_ints(1,2))
+    print(is_go())
     r.sleep();
