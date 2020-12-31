c138d313f4b55bea0a83689772997679dd320e7e
Winnie T <WinnieTrandinh@users.noreply.github.com>
2020-12-31 13:40:52 -0500

    Added services to update current task.

--- src/control/src/control/classifier.py
+++ src/control/src/control/classifier.py
@@ -1,26 +1,37 @@
 #!/usr/bin/env python
 
 import rospy
-from control.srv import isFieldAnalyzed, isFieldAnalyzedResponse
+from control.srv import isFieldAnalyzed, isFieldAnalyzedResponse, setTaskComplete, setTaskCompleteResponse
 from control.msg import classifier_list
 
 class classifier:
     def __init__(self):
         self.pub = rospy.Publisher('classifier_list', classifier_list, queue_size=10)
-        self.serv = rospy.Service('is_field_analyzed', isFieldAnalyzed, self.analyze)
+        self.analyze_serv = rospy.Service('is_field_analyzed', isFieldAnalyzed, self.analyze)
+        self.complete_task_serv = rospy.Service('set_task_complete', setTaskComplete, self.set_task_complete)
         self.objects = []
         self.x = 0
         self.y = 0
         self.obj_class = ""
 
+    # set object as head of list
+    def set_object(self):
+        self.x = self.objects[0].get_x()
+        self.y = self.objects[0].get_y()
+        self.obj_class = self.objects[0].get_obj_class()
+
+    # services
     def analyze(self, req):
     	print('Searching field for what objects are present and where in the field')
-        # set self.x, self.y, self.classes
         obj = object(5,5,"foo")
         self.objects.append(obj)
+        self.set_object()
     	return True
 
-    # add service here to update x, y, class
+    def set_task_complete(self, req):
+        self.objects = self.objects.pop(0)
+        self.set_object()
+
 
 class object:
     def __init__(self, x, y, obj_class):
