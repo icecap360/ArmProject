46cfbc08985d6ffbeb486d59c8f37a35499cca11
Muhammad Ali <icecap360@live.com>
2020-12-31 22:03:02 -0500

    created is on top server

---
+++ src/control/src/control/is_on_top.py
@@ -0,0 +1,60 @@
+#! /usr/bin/env python
+
+import rospy
+import actionlib
+import control.msg
+from control.msg import ensureIsOnTopFeedback, ensureIsOnTopResult, ensureIsOnTopAction
+
+class ensure_is_on_top(object):
+	# create messages that are used to publish feedback/result
+	_feedback = ensureIsOnTopFeedback()
+	_result = ensureIsOnTopResult()
+	_action = ensureIsOnTopAction
+	def __init__(self):
+		self._action_name = 'ensure_is_on_top'
+		self._as = actionlib.SimpleActionServer(self._action_name, self._action, execute_cb=self.execute_cb, auto_start = False)
+		self._as.start()
+		#error tolerance should be a programmable parameter from paramserver
+		self.error_tolerance = 100 
+	
+	def calculate_error(self):
+		error = 0.1
+		pose = 0.5
+		return error,pose
+
+	def execute_cb(self, goal):
+		# helper variables
+		r = rospy.Rate(1)
+		success = True
+		
+		# calculate the current error
+		error = 99999#this should be infinite
+		while (True):
+			error, next_pose = self.calculate_error() 
+			self._feedback.current_error = error
+			self._as.publish_feedback(self._feedback)
+			# publish info to the console for the user
+			rospy.loginfo('The current error is', error)
+		
+			if self.is_preempt_requested():
+				rospy.loginfo('Preempted Arm is on top of object verification')
+				self._as.set_preempted()
+				success = False
+				break
+			elif (error < self.error_tolerance):
+				rospy.loginfo('Within the error tolerance the Arm on top of object')
+				r.sleep()
+				break
+			else:
+				#move robot to next_pose, import helpers from moveit
+				pass
+			
+		if success:
+			self._result.is_on_top = True
+			rospy.loginfo('Is on top verification succeded')
+			self._as.set_succeeded(self._result)
+
+if __name__ == '__main__':
+	rospy.init_node('is_on_top')
+	server = ensure_is_on_top()
+	rospy.spin()
