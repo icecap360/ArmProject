41877acb1d68b45751625d2232dc183cd6dabbf7
Winnie T <WinnieTrandinh@users.noreply.github.com>
2021-01-02 14:16:12 -0500

    calculate_dimension.srv namechange to avoid name conflicts.

--- src/control/src/control/calculate_dimension.py
+++ src/control/src/control/calculate_dimension.py
@@ -2,14 +2,13 @@
 
 import rospy
 from control.msg import object_dimension
-from control.srv import calculateDimension
+from control.srv import calculateDim
 
 class calculateDimension:
     def __init__(self):
         # also subsribe to image and pointcloud2 topics
         self.dimension_topic = rospy.Publisher('object_dimension', object_dimension, queue_size=10)
-        # self.serv = rospy.Service('calculate_dimension', calculateDimension, self.calculate_dimension)
-        self.set_object_serv = rospy.Service('set_object', calculateDimension, self.calculate_dimension)
+        self.serv = rospy.Service('calculate_dimension', calculateDim, self.calculate_dimension)
         self.length = 0
         self.width = 0
         self.height = 0
@@ -29,9 +28,9 @@ class calculateDimension:
 
     # service
     def calculate_dimension(self, req):
-        # self.set_length(5)
-        # self.set_width(5)
-        # self.set_height(5)
+        self.set_length(5)
+        self.set_width(5)
+        self.set_height(5)
         return True
 
 if __name__ == '__main__':
