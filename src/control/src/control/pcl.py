ad80136c522dafe4809269d41d9a17dc43c2fce5
Muhammad Ali <icecap360@live.com>
2021-01-04 14:18:58 -0500

    Ros_numpify removed

--- src/control/src/control/pcl.py
+++ src/control/src/control/pcl.py
@@ -8,14 +8,40 @@ import numpy as np
 
 def callback(data):
     print('yeah')
-    pc = ros_numpy.numpify(data)
-    print(pc.shape[0])
+    pc = pcl.ros_numpy.numpify(data)
+    print(pc.shape)
     points=np.zeros((pc.shape[0],3))
     points[:,0]=pc['x']
     points[:,1]=pc['y']
     points[:,2]=pc['z']
     p = pcl.PointCloud(np.array(points, dtype=np.float32))
 
+def callback(ros_cloud):
+    """ Converts a ROS PointCloud2 message to a pcl PointXYZRGB
+
+        Args:
+            ros_cloud (PointCloud2): ROS PointCloud2 message
+
+        Returns:
+            pcl.PointCloud_PointXYZRGB: PCL XYZRGB point cloud
+    """
+    points_list = []
+    
+    print(ros_numpy.point_cloud2.split_rgb_field(ros_cloud) )
+    print(ros_numpy.point_cloud2.pointcloud2_to_xyz_array(ros_cloud))
+    
+    
+    #print('shape', pcl.ros_numpy.numpify(ros_cloud))#[0][0])
+    for i in rawPoints:
+        for j in i:
+            pass#print(type(j))
+        #print()#type(i))
+    rawPoints = pc2.read_points(ros_cloud, skip_nans=True)
+
+    #for data in :
+    #    points_list.append([data[0], data[1], data[2], data[3]])
+    #pcl_data = pcl.PointCloud_PointXYZRGB()
+    #pcl_data.from_list(points_list)
 
 def listener():
     # In ROS, nodes are uniquely named. If two nodes with the same
