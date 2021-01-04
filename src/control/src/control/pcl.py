df01ff50e0fb36e4156c2632e7996601542357f1
Muhammad Ali <icecap360@live.com>
2021-01-04 15:25:21 -0500

    rgb Image msg publishing

--- src/control/src/control/pcl.py
+++ src/control/src/control/pcl.py
@@ -5,21 +5,37 @@ from sensor_msgs.msg import PointCloud2
 import sensor_msgs.point_cloud2 as pc2
 import ros_numpy
 import numpy as np
+from sensor_msgs.msg import Image
+import numpy.lib.recfunctions as rf
 
-def callback(ros_cloud):
-    """ Converts a ROS PointCloud2 message to a pcl PointXYZRGB
+print(dir(rf))
 
-        Args:
-            ros_cloud (PointCloud2): ROS PointCloud2 message
+class pointCloudPCL:
+    def __init__(self):
+        self.image_pub = rospy.Publisher('depth_image', Image, queue_size=10)
+    def pointCloudToXYZRGB(self, ros_cloud):
+        """ Converts a ROS PointCloud2 message to a pcl PointXYZRGB
 
-        Returns:
-            xyzrgb numpy array
-    """
-    ros_cloud_arr = ros_numpy.point_cloud2.pointcloud2_to_array(ros_cloud) 
-    xyz = ros_numpy.point_cloud2.get_xyz_points(ros_cloud_arr)
-    rgb = ros_numpy.point_cloud2.split_rgb_field(ros_cloud_arr)[['r','g','b']]
-    print(xyz.shape)
-    print(rgb)
+            Args:
+                ros_cloud (PointCloud2): ROS PointCloud2 message
+
+            Returns:
+                xyzrgb numpy array
+        """
+        ros_cloud_arr = ros_numpy.point_cloud2.pointcloud2_to_array(ros_cloud) 
+        xyz = ros_numpy.point_cloud2.get_xyz_points(ros_cloud_arr)
+        rgb = ros_numpy.point_cloud2.split_rgb_field(ros_cloud_arr)[['r','g','b']]
+        r = rgb['r']
+        g = rgb['g']
+        b=rgb['b']
+        img = np.array([r,g,b]) #shape is 3,480,680
+        img = np.moveaxis(img, 0,2)
+        print(img.shape)
+        #better alternative: rf.structured_to_unstructured(rgb)
+        self.image_pub.publish(
+            ros_numpy.image.numpy_to_image(
+                img, "rgb8"))
+
 def listener():
     # In ROS, nodes are uniquely named. If two nodes with the same
     # name are launched, the previous one is kicked off. The
@@ -27,8 +43,8 @@ def listener():
     # name for our 'listener' node so that multiple listeners can
     # run simultaneously.
     rospy.init_node('listener', anonymous=True)
-
-    rospy.Subscriber('/camera/depth/points', PointCloud2, callback)
+    point_coud_pcl = pointCloudPCL()
+    rospy.Subscriber('/camera/depth/points', PointCloud2, point_coud_pcl.pointCloudToXYZRGB)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
