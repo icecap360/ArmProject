9a36b1766a7048f60d3d40b66c7ceb3f3e17d24e
Muhammad Ali <icecap360@live.com>
2021-01-11 15:14:12 -0500

    Adding comments/sections to image segmenter

--- src/control/src/control/image_segmenter.py
+++ src/control/src/control/image_segmenter.py
@@ -24,32 +24,16 @@ class imageSegmenter:
         self.net = cv2.dnn.readNetFromDarknet(self.config_path, self.weight_path)
         self.labels = open(self.labels_path).read().strip().split('\n')
         self.execute = False #set this to false so node does not initially execute
-        # All SERVICES and TOPICS MUST be created BELOW
+        # All SERVICES and TOPICS MUST be created BELOW, after the net is loaded
         self.sub = rospy.Subscriber('/camera/depth/points', PointCloud2, self.classify_img_subcb)
         self.serv = rospy.Service('get_image_hulls', doService, self.get_image_hulls_srvcb)
         self.pub = rospy.Publisher('image_hulls', image_points, queue_size=1)
         self.image_pub = rospy.Publisher('arm_vision_image', Image, queue_size=1)
-
-    ######### CALLBACKS #########
-    def get_image_hulls_srvcb(self, req):
-        self.execute=True
-        return True
-    def classify_img_subcb(self, ros_cloud):
-        if not self.execute:
-            return
-        ros_cloud_arr = ros_numpy.point_cloud2.pointcloud2_to_array(ros_cloud)
-        xyz = ros_numpy.point_cloud2.get_xyz_points(ros_cloud_arr, remove_nans=False)
-        rgb = ros_numpy.point_cloud2.split_rgb_field(ros_cloud_arr)
-        img = np.array([rgb['r'],rgb['g'],rgb['b']]) #shape is 3,480,640
-        img = np.moveaxis(img, 0, 2) #shape is 480,640,3
-        self.height, self.width = img.shape[:2]
-        boxes, confidences, classes = self.make_prediction(self.net, self.labels,img,self.confidence,self.threshold)
-        image_segments = self.make_contour_hulls(img, boxes)
-        image_segments = self.index_to_xy(image_segments, xyz)
-        self.pub_predictions(image_segments,classes)
-        self.execute = False #this must be false
-        print("Finished making image segmentations")
-
+    
+    """ The different sections of the algorithm are controlled by the callbacks
+        Each section contains a main function, and helpers below it (breaking the conventions for readibility)."""
+    
+    ########### OBJECT RECOGNITION ALGORITHM (MACHINE LEARNING) #########
     def make_prediction(self, net, labels, image, confidence, threshold):
         layer_names = net.getLayerNames()
         layer_names = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
@@ -88,6 +72,7 @@ class imageSegmenter:
     def subset_detections(self, l , indexes):
         return [l[i] for i in indexes]
 
+    ########## CONTOURS AND HULLS ###########
     def make_contour_hulls(self, img, boxes):
         hulls = []
         for box in boxes:
@@ -146,6 +131,9 @@ class imageSegmenter:
         row_ind = max(row_ind,0)
         return row_ind, col_ind
 
+    ########## CONVERTING IMAGE INDEXES TO XY #######
+        # if there is a problem, it is likely here, is xyz in [x,y,z] form of [z,y,x]
+        # is first index the col or the row (I think it is because of error and previous code)
     def index_to_xy(self, hull_indexes, xyz):
         hulls_coords = []
         for hull in hull_indexes:
@@ -159,9 +147,9 @@ class imageSegmenter:
         col = point[0]
         row = point[1]
         return [xyz[row][col][0], xyz[row][col][1]]
-        
+
+    ####### PUBLISHING MSG #######   
     def pub_predictions(self, image_segments, classes):
-        # Turns the raw bounding boxes and classes into a msg, and publishes it
         msg = image_points()
         msg.x, msg.y, msg.obj_class = [],[],[]
         end_of_det_num = float('inf') #this should come from the param server
@@ -179,6 +167,25 @@ class imageSegmenter:
             msg.obj_class.append(end_of_det_str)
         self.pub.publish(msg)
 
+    ######### CALLBACKS #########
+    def classify_img_subcb(self, ros_cloud):
+        if not self.execute:
+            return
+        ros_cloud_arr = ros_numpy.point_cloud2.pointcloud2_to_array(ros_cloud)
+        xyz = ros_numpy.point_cloud2.get_xyz_points(ros_cloud_arr, remove_nans=False)
+        rgb = ros_numpy.point_cloud2.split_rgb_field(ros_cloud_arr)
+        img = np.array([rgb['r'],rgb['g'],rgb['b']]) #shape is 3,480,640
+        img = np.moveaxis(img, 0, 2) #shape is 480,640,3
+        self.height, self.width = img.shape[:2]
+        boxes, confidences, classes = self.make_prediction(self.net, self.labels,img,self.confidence,self.threshold)
+        image_segments = self.make_contour_hulls(img, boxes)
+        image_segments = self.index_to_xy(image_segments, xyz)
+        self.pub_predictions(image_segments,classes)
+        self.execute = False #this must be false
+        print("Finished making image segmentations")
+    def get_image_hulls_srvcb(self, req):
+        self.execute=True
+        return True
 
 if __name__ == '__main__':
     rospy.init_node('image_segmenter', anonymous=True)
