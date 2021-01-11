68332939249762d5c56c8bf7099bee3e91dcb8e9
Muhammad Ali <icecap360@live.com>
2021-01-10 21:05:01 -0500

    tried to get points inside the interior, but the polygon shape is incorrect, see screen shot

--- src/control/src/control/image_segmenter.py
+++ src/control/src/control/image_segmenter.py
@@ -7,6 +7,8 @@ from sensor_msgs.msg import PointCloud2, Image
 from control.msg import image_points
 from control.srv import doService
 import cv2
+import random
+from shapely.geometry import Polygon, Point
 # import os
 # import time
 import rospkg
@@ -74,7 +76,18 @@ class imageSegmenter:
         #self.image_pub.publish(
         #    ros_numpy.image.numpy_to_image(
         #        imageBounded, "rgb8"))
-
+    def find_points_inside(self, hull, nx=25, ny=25):
+        poly = Polygon(np.squeeze(hull))
+        minx, miny, maxx, maxy = poly.bounds
+        x = np.linspace(minx,maxx,nx)
+        y = np.linspace(miny,maxy,ny)
+        print np.squeeze(hull).shape
+        interior_points = []
+        for i in x:
+            for j in y:
+                if cv2.pointPolygonTest(hull, (i,j), False)>0:
+                    interior_points.append([[i, j]])
+        return np.array(interior_points, dtype=np.int32)
     def make_contour_hulls(self):
         hulls = []
         for box in self.boxes:
@@ -100,19 +113,18 @@ class imageSegmenter:
             thresh = cv2.threshold(gray, 100 ,255, cv2.THRESH_BINARY)[1] #125 is threshold online, it did not work, so I choose 100
             edged=cv2.Canny(thresh,30,200)
             #Now find all the contours
-            contours, hierarchy=cv2.findContours(edged,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+            contours, hierarchy=cv2.findContours(edged,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
             #There is only one object, so all the contours have to do with one object            
             all_contours = np.concatenate(contours)
             #Using convex hulls, alot of the interior shape is lost, so instaed a polydon was formed
-            epsilon = 0.003*cv2.arcLength(all_contours,True) # 0.003 gives 90 points, 0.005 gives 70
+            epsilon = 0.0004*cv2.arcLength(all_contours,True) # 0.001 gives 90 points, 0.005 gives 70
             hull = cv2.approxPolyDP(all_contours,epsilon,True)
-            print hull.size
+            interior_hull = self.find_points_inside(hull)
+            print interior_hull.shape
             #draw the contours and publish the image
             image_cropped = cv2.UMat(image_cropped)
-            cv2.drawContours(image_cropped,[hull],-1,(0,255,0),3)
+            cv2.drawContours(image_cropped,interior_hull,-1,(0,255,0),15)
             image_cropped = image_cropped.get()
-            print 'Number of contours: ', len(contours)
-            print 'Hierarchy: ', hierarchy 
             
             self.image_pub.publish(
                 ros_numpy.image.numpy_to_image(
